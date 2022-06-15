ce651ccb4611b77319414de6f4a537d686239e61
A <A@DESKTOP-O2QD961>
2022-06-15 11:54:51 +0530

    By Meenal

--- GitHub/src/Demo.java
+++ GitHub/src/Demo.java
@@ -1,8 +1,14 @@
 
-public class Demo 
+public class Demo
 {
-   public static void main(String[] args) 
-   {
-	   System.out.println("Welcome To GitHub");
-   }
+
+	public void m1()
+	{
+        System.out.println("m1 method of Demo class");
+	}
+
+	public static void main(String[] args) 
+	{
+		System.out.println("Welcome To GitHub");
+	}
 }
